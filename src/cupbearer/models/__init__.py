eb243b5d860b6d735471917c49ac99419bcadbe1
oliveradk <danielskoch.oliver@gmail.com>
2024-04-24 17:10:59 -0700

    pass load kwargs

--- src/cupbearer/models/__init__.py
+++ src/cupbearer/models/__init__.py
@@ -8,12 +8,12 @@ from .models import CNN, MLP, PreActResNet
 from .transformers_hf import TamperingPredictionTransformer
 
 
-def load(model: HookedModel, path: Path | str):
+def load(model: HookedModel, path: Path | str, **load_kwargs):
     path = Path(path)
     # Our convention is that LightningModules store the actual pytorch model
     # as a `model` attribute. We use the last checkpoint (generated via the
     # save_last=True option to the ModelCheckpoint callback).
-    state_dict = torch.load(path / "checkpoints" / "last.ckpt")["state_dict"]
+    state_dict = torch.load(path / "checkpoints" / "last.ckpt", **load_kwargs)["state_dict"]
     # We want the state_dict for the 'model' submodule, so remove
     # the 'model.' prefix from the keys.
     state_dict = {k[6:]: v for k, v in state_dict.items() if k.startswith("model.")}

8cb14e5804bab1751e7b08d91a1ce487eb18246a
David Johnston <davidoj@fastmail.com.au>
2024-05-30 00:09:51 +0000

    use Eleuther models instead of ejenner

--- src/cupbearer/tasks/quirky_lm.py
+++ src/cupbearer/tasks/quirky_lm.py
@@ -37,8 +37,8 @@ def quirky_lm(
     if random_names:
         model_name += "-random-names"
 
-    if dataset == 'sciq':
-        model_name = f"ejenner/quirky_sciq_mistral7b_{mixture_str}_{name_str}"        
+    # if dataset == 'sciq':
+    #     model_name = f"ejenner/quirky_sciq_mistral7b_{mixture_str}_{name_str}"        
 
     model = None
     tokenizer = None
@@ -53,12 +53,12 @@ def quirky_lm(
 
     dataset_name = dataset
 
-    if mixture and dataset == "sciq":
-        raw_dataset = load_dataset(f"ejenner/quirky_{dataset_name}_raw")
-    else:
+    # if mixture and dataset == "sciq":
+    #     raw_dataset = load_dataset(f"ejenner/quirky_{dataset_name}_raw")
+    # else:
         # The older non-mixture checkpoints from above where trained on a slightly
         # different distribution---shouldn't matter much but we'll just use that.
-        raw_dataset = load_dataset(f"EleutherAI/quirky_{dataset_name}_raw")
+    raw_dataset = load_dataset(f"EleutherAI/quirky_{dataset_name}_raw")
 
     dataset = templatize_quirky_dataset(
         raw_dataset,
